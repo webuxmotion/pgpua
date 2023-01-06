e918d4aac5f75dbfb820a62b8f90d73c48bef8fb
Pereverziev Andrii <a.pereverziev@enamine.net>
2023-01-06 13:42:14 +0200

    Show projects list

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const app = express()
 const path = require('path')
 const port = 5000
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-})
-
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 
 app.listen(port, () => {
